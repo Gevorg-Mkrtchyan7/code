beaf33c79dbc1d8bd3393ef0199b23e214354524
Gevorg-Mkrtchyan7 <gevorgmkrtchyan744@gmail.com>
2025-03-09 02:42:35 +0400

    areci

---
+++ foo.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+	int a = 0;
+	printf("Enter your number \n");
+	scanf("%d", &a);	
+
+	if(a > 10){
+	printf("Your number is bigger 10 \n");
+	}else{
+	printf("your number is lower 10 \n");
+	}
+}
